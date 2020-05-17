d2915819b62c8d5c22457311af53a16af10ec556
Wayne Martin <waymart23@gmail.com>
2020-05-17 03:33:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,5 @@ def introduction(name = "Josh")
 end
 
 def introduction_with_language(name= "Dan!", language = "ember.js")
-  puts "Hi, my name is #{name} and I am
-  learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
